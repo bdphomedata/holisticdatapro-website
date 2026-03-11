0000894293a6094d90b7912c93ccd59ef2d9914a
bdphomedata <bartusdepaiva@bdphd.com>
2026-03-11 09:44:34 +0200

    Update subscribe_process.php

--- subscribe_process.php
+++ subscribe_process.php
@@ -1,31 +1,30 @@
 <?php
+
 $serverName = "bdphdwebsite.database.windows.net";
+
 $connectionOptions = array(
 "Database" => "BDPHD",
 "Uid" => "BDPHD",
 "PWD" => "HDLinkMaster001!"
 );
 
-// Connect to Azure SQL
 $conn = sqlsrv_connect($serverName, $connectionOptions);
 
 if ($conn === false) {
 die(print_r(sqlsrv_errors(), true));
 }
 
-// Capture data from your HTML form
 $first_name   = $_POST['first_name'];
 $surname      = $_POST['surname'];
-$email        = $_POST['email']; // Make sure your HTML has <input name="email">
+$email        = $_POST['email'];
 $gender       = $_POST['gender'];
 $ethnic_group = $_POST['ethnic_group'];
 $country      = $_POST['country'];
 
-// SQL Query to insert the new member
-$tsql = "INSERT INTO Subscribers (FirstName, Surname, Email, Gender, EthnicGroup, Country)
-VALUES (?, ?, ?, ?, ?, ?)";
+$tsql = "INSERT INTO Subscribers (FirstName, Surname, Email, Gender, EthnicGroup, Country) VALUES (?, ?, ?, ?, ?, ?)";
 
 $params = array($first_name, $surname, $email, $gender, $ethnic_group, $country);
+
 $stmt = sqlsrv_query($conn, $tsql, $params);
 
 if ($stmt === false) {
@@ -40,5 +39,7 @@ echo "Success! Welcome to the Network.";
 }
 
 sqlsrv_free_stmt($stmt);
+
 sqlsrv_close($conn);
+
 ?>
